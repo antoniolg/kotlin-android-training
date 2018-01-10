3a1f9a75ba13f494ad00a15449d169b4ea4ed60a
Antonio Leiva <antonioleivag@gmail.com>
2018-01-10 09:51:01 +0100

    Extension functions

--- app/src/main/java/com/antonioleiva/kotlinandroidtraining/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/kotlinandroidtraining/MainActivity.kt
@@ -2,7 +2,6 @@ package com.antonioleiva.kotlinandroidtraining
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        submit.setOnClickListener {
-            val message = "user: ${username.text}, pass: ${password.text}"
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        }
+        submit.setOnClickListener { toast("user: ${username.text}, pass: ${password.text}") }
     }
 }
