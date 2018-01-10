bf10ebbdef8a2da046a8d2c3f61b5b8346171666
Antonio Leiva <antonioleivag@gmail.com>
2018-01-10 09:47:35 +0100

    Kotlin Android Extensions

--- app/src/main/java/com/antonioleiva/kotlinandroidtraining/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/kotlinandroidtraining/MainActivity.kt
@@ -2,11 +2,18 @@ package com.antonioleiva.kotlinandroidtraining
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submit.setOnClickListener {
+            val message = "user: ${username.text}, pass: ${password.text}"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
